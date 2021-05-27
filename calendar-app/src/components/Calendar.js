d2a21d7d65095eee6e58ffe907f82c3fb1736133
ewalis1 <73714272+ewalis1@users.noreply.github.com>
2021-05-27 22:19:32 +0200

    added useState, useEffect and styling for a selected day

--- calendar-app/src/components/Calendar.js
+++ calendar-app/src/components/Calendar.js
@@ -1,28 +1,36 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import moment from 'moment';
 import localization from 'moment/locale/pl';
 
 export default function Calendar() {
-  const value = moment()
+  const [value, setValue] = useState(moment());
+  const [calendar, setCalendar] = useState([]);
   moment.locale('pl', localization);
+
   const startWeek = value.clone().startOf("month").startOf("week");
   const endWeek = value.clone().endOf("month").endOf("week");
-  const day = startWeek.clone().subtract(1, "day");
-
-  const calendar = [];
-
-  while(day.isBefore(endWeek, "day")) {
-    calendar.push(
-      Array(7).fill("d").map(() => day.add(1, "day").clone())
-    );
-  }
-
+  
+  useEffect(() => {
+    const day = startWeek.clone().subtract(1, "day");
+    const arr = [];
+    while(day.isBefore(endWeek, "day")) {
+      arr.push(
+        Array(7).fill("d").map(() => day.add(1, "day").clone())
+      );
+    }
+    setCalendar(arr);
+  }, [value]);
+  
   return <div className="calendar">
     {
       calendar.map((week, index) => (
         <div key={index}>
           {week.map((day, index) => (
-            <div key={index} className="day">{day.format("D")}</div>
+            <div key={index} className="day" onClick={() => setValue(day)}>
+              <div className={value.isSame(day, "day") ? "chosenDay" : ""}>
+                {day.format("DD")}
+              </div>
+            </div>
           ))}
         </div>
       ))
